1845a2524426ec627bc5da6576c908377b83c550
Jean Zapata <jpzapata1@utpl.edu.ec>
2023-10-22 20:25:37 -0500

    primer commit

---
+++ proyecto01/src/main/java/com/mycompany/proyecto01/Proyecto01.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.proyecto01;
+
+/**
+ *
+ * @author Jean Zapata
+ */
+public class Proyecto01 {
+
+    public static void main(String[] args) {
+        System.out.println(" Ejemplo básico de java \n");
+        System.out.println("Versionado a travez de Git \n");
+        System.out.println("Manejando desde GitHub \n");
+        System.out.println("Recuerde usar la tenica \n");
+    }
+}
